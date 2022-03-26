09d0272b5eb46f7a3be2e99a86dfdf859ede226c
geekbim <manyuabim9@gmail.com>
2022-03-27 05:59:48 +0700

    feat(class): example inheritance

--- build/class.js
+++ build/class.js
@@ -10,9 +10,30 @@ class User {
     // 2
     constructor(name) {
         this.name = name;
+        this.getName = () => {
+            return this.name;
+        };
+    }
+    setName(value) {
+        this.name = value;
     }
 }
 exports.User = User;
 let user = new User("Abim");
 console.log(user);
 console.log(user.name);
+class Admin extends User {
+    constructor() {
+        super(...arguments);
+        this.read = true;
+        this.write = true;
+        this.getRole = () => {
+            return { read: true, write: true };
+        };
+    }
+}
+let admin = new Admin("Abim");
+console.log(admin);
+console.log(admin.name);
+admin.setName("jarwo");
+console.log(admin.getName());
--- src/class.ts
+++ src/class.ts
@@ -8,8 +8,31 @@ export class User {
 
     // 2
     constructor(public name: string) {}
+
+    setName(value: string): void {
+        this.name = value
+    }
+
+    getName = (): string => {
+        return this.name
+    }
 }
 
 let user = new User("Abim")
 console.log(user)
 console.log(user.name)
+
+class Admin extends User {
+    read: boolean = true
+    write: boolean = true
+
+    getRole = (): {read: boolean, write: boolean} => {
+        return {read: true, write: true}
+    }
+}
+
+let admin = new Admin("Abim")
+console.log(admin)
+console.log(admin.name)
+admin.setName("jarwo")
+console.log(admin.getName())
